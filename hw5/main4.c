800645162aef52388f2a8a82773135e8f0855c54
mnbvcxz1209 <151009588+mnbvcxz1209@users.noreply.github.com>
2023-12-19 14:38:03 +0800

    Create main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int i=12345;
+    
+    int unit=i/1%10;
+    int ten=i/10%10;
+    int hundred=i/100%10;
+    int thousand=i/1000%10;
+    int upper=i/10000;
+    
+    printf("switch result=%d",upper*10000+unit*1000+hundred*100+ten*10+thousand);
+
+    return 0;
+}
