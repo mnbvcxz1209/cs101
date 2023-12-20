e7aa39a7fd4312d9b4a85771fb1e07453685362e
mnbvcxz1209 <151009588+mnbvcxz1209@users.noreply.github.com>
2023-12-20 19:24:12 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -3,18 +3,19 @@
 int main()
 {
     double mypi=4.0;
-    long fixpi=3141592;
     long calcpi=1;
     int i;
     float sign=1.0;
     
-    for(i=1;i<=1000000&&fixpi!=calcpi;i++)
+    for (i=1; i<=1000000 && (calcpi>3141594 || calcpi<3141590) ; i++)
     {
-        mypi=mypi-sign*4.0/(2*i+1);
-        sign=sign*(-1);
-        calcpi=(long)(mypi*1000000);
+        mypi = mypi-sign*4.0/(2*i+1);
+        sign = sign*(-1);
+        calcpi = (long)(mypi*1000000);
     }
-    printf("times=%d\tvalue=%7.5f\n",i,mypi);
+    
+    printf("times=%d \tvalue=%7.5f \n",i,mypi);
     
     return 0;
 }
+
